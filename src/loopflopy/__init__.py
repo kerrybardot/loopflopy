92570502851308d0ca64d6c96a0160b41d2535e0
oscarfasanchez <oscarfasanchez@gmail.com>
2025-10-06 21:18:32 +0800

    added a standar simnple text on init file

---
+++ src/loopflopy/__init__.py
@@ -0,0 +1,8 @@
+from importlib import metadata as _metadata
+
+try:
+    __version__ = _metadata.version("loopflopy")
+except _metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+
+__all__ = ["__version__"]
